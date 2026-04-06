141fca584a8edc46e70ed41d82988ce47a04e669
Cosmo <cosmorollaire@gmail.com>
2026-04-06 13:10:14 -0400

    Create mapping.lua

---
+++ mapping.lua
@@ -0,0 +1,385 @@
+return {
+	toonsandtwisteds = {
+		astro = "Astro",
+		bassie = "Bassie",
+		blot = "Blott",
+		bobette = "Bobette",
+		boxten = "Boxten",
+		brightney = "Brightney",
+		brusha = "Brusha",
+		coal = "Coal",
+		cocoa = "Cocoa",
+		connie = "Connie",
+		cosmo = "Cosmo",
+		dandy = "Dandy",
+		dyle = "Dyle",
+		eclipse = "Eclipse",
+		eggson = "Eggson",
+		finn = "Finn",
+		flutter = "Flutter",
+		flyte = "Flyte",
+		gigi = "Gigi",
+		ginger = "Ginger",
+		glisten = "Glisten",
+		goob = "Goob",
+		gourdy = "Gourdy",
+		looey = "Looey",
+		pebble = "Pebble",
+		poppy = "Poppy",
+		["razzle & dazzle"] = "RazzleDazzle",
+		ribecca = "Ribecca",
+		rodger = "Rodger",
+		rudie = "Rudie",
+		scraps = "Scraps",
+		shelly = "Shelly",
+		shrimpo = "Shrimpo",
+		soulvester = "Soulvester",
+		sprout = "Sprout",
+		squirm = "Squirm",
+		teagan = "Teagan",
+		tisha = "Tisha",
+		toodles = "Toodles",
+		vee = "Vee",
+		yatta = "Yatta"
+	},
+
+	toonskins = {
+		astro = {
+			["starry night"] = "AstroStar",
+			["sleepy carebear"] = "CareBear_Astro",
+			["startime"] = "DreamAstro",
+			["scarlet night"] = "Halloween_Astro",
+			["north star"] = "NorthStar",
+			["vintage"] = "VintageAstro",
+		},
+
+		bassie = {
+			["belle basket"] = "BelleBasket",
+			["vintage"] = "VintageBassie",
+			["springtime"] = "SpringtimeBassie",
+			["sunflower patch"] = "SunflowerPatch",
+			["golden basket"] = "GoldenBasket",
+			["egg hunting"] = "EggHunting",
+		},
+
+		blot = {
+			["cozy pink"] = "CozyPink",
+			["penguin"] = "PenguinBlott",
+			["sanguine silence"] = "SanguineSilence",
+			["showtime"] = "ShowtimeBlott",
+			["vintage"] = "VintageBlott",
+		},
+
+		bobette = {
+			["eve sleeper"] = "EveSleeper",
+			["golden bauble"] = "GoldenBauble",
+			["holly lace"] = "HollyLace",
+			["hot chocolate"] = "HotChocolate",
+			["snowtime"] = "SnowtimeBobette",
+			["vintage"] = "VintageBobette",
+		},
+
+		boxten = {
+			["cloudy dreams"] = "CloudyDream",
+			["creepy chord"] = "CreepyChord",
+			["quiet caroler"] = "QuietCaroler",
+			["passionate wisteria"] = "SunlitNeon",
+			["vintage"] = "VintageBoxten",
+			["winged chords"] = "WingedChords",
+		},
+
+		brightney = {
+			["antique shade"] = "AntiqueShade",
+			["vintage"] = "VintageBrightney",
+			["cozy lighting"] = "CozyLighting",
+			["bright dawn"] = "BrightDawn",
+		},
+
+		brusha = {
+			["autumn palette"] = "AutumnPalette",
+			["figure painter"] = "FigurePainter",
+			["gloomy sleeper"] = "GloomySleeper",
+			["scary stylish"] = "ScaryStylish",
+			["vintage"] = "VintageBrusha",
+			["eggcelent decorator"] = "EggcelentDecorator",
+		},
+
+		coal = {
+			["golden dust"] = "GoldenDust",
+			["pretty"] = "PresentCoal",
+			["snowtime"] = "SnowtimeCoal",
+			["snowy rescue"] = "SnowyRescue",
+			["vintage"] = "VintageCoal",
+		},
+
+		cocoa = {
+			["spring outing"] = "SpringOuting",
+			["vintage"] = "VintageCocoa",
+			["springtime"] = "SpringtimeCocoa",
+			["strawberry paws"] = "StrawberryPaws",
+			["wildberry delight"] = "WildberryDelight",
+			["golden bunny"] = "GoldenBunny",
+		},
+
+		connie = {
+			["brisk breeze"] = "BriskBreeze",
+			["christmas past"] = "ChristmasPast",
+			["frosty mittens"] = "FrostyMittens",
+			["ghastly glow"] = "GhastlyGlow",
+			["lilac haunting"] = "LilacHaunting",
+			["vintage"] = "VintageConnie",
+		},
+
+		cosmo = {
+			["baker"] = "CosmoBaker",
+			["berry"] = "CosmoBerry",
+			["dessertful scientist"] = "DessertfulScientist",
+			["snowtime"] = "SnowtimeCosmo",
+			["sweet roll"] = "SweetRoll",
+			["vintage"] = "VintageCosmo",
+			["carrot cake"] = "CarrotCake",
+		},
+
+		dandy = {
+			["christmas"] = "ChristmasDandy",
+			["easter"] = "EasterDandy",
+		},
+
+		dyle = {
+			["christmas"] = "HolidayDyle",
+			["easter"] = "EasterDyle",
+		},
+
+		eclipse = {
+			["full moon"] = "FullMoon",
+			["little red"] = "LittleRed",
+			["lone wolf"] = "LoneWolf",
+			["vintage"] = "VintageEclipse",
+		},
+
+		eggson = {
+			["painted patterns"] = "PaintedPatterns",
+			["vintage"] = "VintageEggson",
+			["still hip"] = "StillHip",
+			["springtime"] = "SpringtimeEggson",
+			["golden egg"] = "GoldenEgg",
+		},
+
+		finn = {
+			["prismatic pal"] = "FishFinn",
+			["ice fisher"] = "IceFisher",
+			["tacky sweater"] = "TackySweater",
+			["vintage"] = "VintageFinn",
+			["flowery punch"] = "FloweryPunch",
+		},
+
+		flutter = {
+			["frosty fluff"] = "FrostyFluff",
+			["vintage"] = "VintageFlutter",
+			["vivid monarch"] = "VividMonarch",
+			["springtime"] = "SpringtimeFlutter",
+			["rosy maple moth"] = "RosyMapleMoth",
+		},
+
+		flyte = {
+			["spring sky"] = "SpringSky",
+			["vintage"] = "VintageFlyte",
+			["golden wings"] = "GoldenWings",
+			["luna moth"] = "LunaMoth",
+			["springtime"] = "SpringtimeFlyte",
+		},
+
+		gigi = {
+			["rainy day"] = "RainyDay",
+			["snowball fight"] = "SnowballFight",
+			["vintage"] = "VintageGigi",
+		},
+
+		ginger = {
+			["minty pine"] = "GingerMinty",
+			["golden cookie"] = "GoldenCookie",
+			["new years"] = "NewYearsGinger",
+			["snowtime ginger"] = "SnowtimeGinger",
+			["sugar cookie"] = "SugarCookie",
+			["vintage"] = "VintageGinger",
+		},
+
+		glisten = {
+			["crimson imp"] = "CrimsonImp",
+			["sparkling snow"] = "SparklingSnow",
+			["sweater"] = "Sweater",
+			["vintage"] = "VintageGlisten",
+			["wrapped chic"] = "WrappedChic",
+		},
+
+		goob = {
+			["party hat"] = "PartyHat",
+			["special spaghetti"] = "SpecialSpaghetti",
+			["vintage"] = "VintageGoob",
+			["neapolitan"] = "Neapolitan",
+		},
+
+		gourdy = {
+			["crinkling leaves"] = "CrinklingLeaves",
+			["fresh fall"] = "FreshFall",
+			["lost prince"] = "LostPrince",
+			["midnight sleeper"] = "MidnightSleeper",
+			["vintage"] = "VintageGourdy",
+		},
+
+		looey = {
+			["creepy clown"] = "CreepyClown",
+			["dapperly festive"] = "DapperlyFestive",
+			["marching band"] = "MarchingBand",
+			["showtime looey"] = "ShowtimeLooey",
+			["swirling mix up"] = "SwirlingMixUp",
+			["vintage"] = "VintageLooey",
+			["funny bunny"] = "FunnyBunny",
+		},
+
+		pebble = {
+			["startime"] = "DreamPebble",
+			["extra cheese"] = "ExtraCheese",
+			["loyal bud"] = "FlowerBud",
+			["gemstone"] = "GemPebble",
+			["gleeful gift"] = "GleefulGift",
+			["served eggy"] = "PebbleEgged",
+			["vintage"] = "VintagePebble",
+			["bunny wabbit"] = "BunnyWabbit",
+		},
+
+		poppy = {
+			["sapphire"] = "BluePoppy",
+			["bowful beauty"] = "BowfulBeauty",
+			["confident caroler"] = "ConfidentCaroler",
+			["double bubble"] = "DoubleBubble",
+			["picnic outing"] = "PicnicOuting",
+			["vintage"] = "VintagePoppy",
+		},
+
+		["razzle & dazzle"] = {
+			["aquaraz"] = "AquaRaz",
+			["cozy christmas"] = "CozyChristmas",
+			["giftwrap and ribbons"] = "GiftwrapAndRibbons",
+			["matcha morning"] = "MatchaMorning",
+			["radiant dream/reworked"] = "RadiantDream",
+			["showtime"] = "ShowtimeRazzleDazzle",
+			["vintage"] = "VintageRazzleDazzle",
+			["spring formal"] = "SpringFormal",
+		},
+
+		ribecca = {
+			["haunted topic"] = "HauntedTopic",
+			["skelekitty"] = "SkeleKitty",
+			["vintage"] = "VintageRibecca",
+		},
+
+		rodger = {
+			["ebony detective"] = "AceDetective",
+			["alpha wolf"] = "AlphaWolf",
+			["festive detective"] = "FestiveDetective",
+			["list maker"] = "ListMaker",
+			["vintage"] = "VintageRodger",
+			["research mobile"] = "ResearchMobile",
+		},
+
+		rudie = {
+			["golden"] = "GoldenAntlers",
+			["holly jolly"] = "HollyJolly",
+			["blue bell"] = "RudieBlueBell",
+			["snowtime"] = "SnowtimeRudie",
+			["vintage"] = "VintageRudie",
+		},
+
+		scraps = {
+			["cutesy calico"] = "CutesyCalico",
+			["pastel paper"] = "PastelPaper",
+			["vintage"] = "VintageScraps",
+			["rainbow sherbert"] = "RainbowSherbet",
+		},
+
+		shelly = {
+			["cheer carebear"] = "CareBear_Shelly",
+			["dream shelly"] = "DreamShelly",
+			["fall stroll"] = "FallStroll",
+			["prehistoric peppermint"] = "PrehistoricPeppermint",
+			["dino snore"] = "ShellyDino",
+			["vintage"] = "VintageShelly",
+			["violet caster"] = "VioletCaster",
+		},
+
+		shrimpo = {
+			["bundled up"] = "BundledUp",
+			["grumpy carebear"] = "CareBear_Shrimpo",
+			["cranky christmas"] = "CrankyChristmas",
+			["fairy godmother"] = "FairyGodmother",
+			["punk"] = "PunkShrimpo",
+			["vintage"] = "VintageShrimpo",
+			["wilted daisies"] = "WiltedDaisies",
+		},
+
+		soulvester = {
+			["gourd guard"] = "GourdGuard",
+			["haunted flame"] = "HauntedFlame",
+			["vintage"] = "VintageSoulvester",
+		},
+
+		sprout = {
+			["baker"] = "BakerSprout",
+			["berry beast"] = "BerryBeast",
+			["choco berry"] = "ChocoBerry",
+			["startime"] = "DreamSprout",
+			["sugary shortcake"] = "SugaryShortcake",
+			["vintage"] = "VintageSprout",
+			["tasty carrot"] = "TastyCarrot",
+		},
+
+		squirm = {
+			["vintage"] = "VintageSquirm",
+			["spring quilt"] = "SpringQuilt",
+		},
+
+		teagan = {
+			["fancy dress"] = "FancyDress",
+			["shining wreath"] = "ShiningWreath",
+			["vintage"] = "VintageTeagan",
+			["pastel porcelain"] = "PastelPorcelain",
+		},
+
+		tisha = {
+			["maid"] = "MaidTisha",
+			["vintage"] = "VintageTisha",
+			["winter mint"] = "WinterMint",
+			["spring cleaning"] = "SpringCleaning",
+		},
+
+		toodles = {
+			["candy critter"] = "CandyCritter",
+			["dog plush onesie"] = "DogPlushOnesie",
+			["festive dress"] = "FestiveDress",
+			["jr detective"] = "JrDetective",
+			["little helper"] = "LittleHelper",
+			["vintage"] = "VintageToodles",
+		},
+
+		vee = {
+			["startime"] = "DreamVee",
+			["holiday special"] = "HolidaySpecial",
+			["showtime"] = "ShowtimeVee",
+			["space"] = "VeeSpace",
+			["vintage"] = "VintageVee",
+			["zapped creation"] = "ZappedCreation",
+			["veester bunny"] = "VeesterBunny",
+		},
+
+		yatta = {
+			["colorful april/reworked"] = "April1",
+			["blossom buddy"] = "BlossomBuddy",
+			["choco cones"] = "ChocoCones",
+			["guess what i eat"] = "EatBricks",
+			["infected candy"] = "InfectedCandy",
+			["showtime"] = "ShowtimeYatta",
+			["vintage"] = "VintageYatta",
+		},
+	}
+}
