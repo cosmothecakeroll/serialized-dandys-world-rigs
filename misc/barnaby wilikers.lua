333a2984edd84dc972232dcace4747ee50aaeef8
Cosmo <cosmorollaire@gmail.com>
2026-04-10 20:16:02 -0400

    Create barnaby wilikers.lua

---
+++ misc/barnaby wilikers.lua
@@ -0,0 +1,7 @@
+local barnabyWilikers = game:GetService("InsertService"):CreateMeshPartAsync("rbxassetid://117631321173872", Enum.CollisionFidelity.Default, Enum.RenderFidelity.Automatic)
+barnabyWilikers.TextureID = "rbxassetid://98267534680981"
+barnabyWilikers.Name = "Barnaby Wilikers"
+barnabyWilikers.Size = Vector3.new(0.906, 0.6374, 0.4227)
+barnabyWilikers.CFrame = CFrame.new(Vector3.yAxis * 5)
+
+return barnabyWilikers
